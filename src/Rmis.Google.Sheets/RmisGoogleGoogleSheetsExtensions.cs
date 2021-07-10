4d1174fe1c05001047f2984974e864520bbdfbb7
Vladimir <metalistiks@hotmail.com>
2021-07-10 21:00:04 +0300

    #670 Организован доступ к google sheets для получения расписаиня.

---
+++ src/Rmis.Google.Sheets/RmisGoogleGoogleSheetsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Rmis.Google.Sheets.Abstract;
+
+namespace Rmis.Google.Sheets
+{
+    public static class RmisGoogleGoogleSheetsExtensions
+    {
+        public static IServiceCollection AddRmisGoogleGoogleSheets(this IServiceCollection services)
+        {
+            return services.AddSingleton<IGoogleSheetsScheduleProvider, GoogleSheetsScheduleProvider>();
+        }
+    }
+}
